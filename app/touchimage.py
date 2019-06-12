f1a9d69d91f126ee7584bd916e78ce2026babea1
swissembedded <daniel.haensse@swissembedded.com>
2019-06-12 07:01:37 +0200

    added click for touchimage

--- app/touchimage.py
+++ app/touchimage.py
@@ -61,7 +61,7 @@ class TouchImage(Image):
                             if inspectionside=="Top":
                                 #rotate=rotation-part['Rotation']
                                 #Rotate(angle=rotation-part['Rotation'], origin=self.center)
-                                
+
                                 Rotate(angle=rotationp, origin=self.center)
 
                                 Rectangle(pos=(xp+posxp, yp+posyp), size=(part['BodySize'][0]*scale, part['BodySize'][1]*scale))
@@ -76,5 +76,54 @@ class TouchImage(Image):
 
     def on_touch_down(self, touch):
         ### mouse down event
-        # maybe needed in future revision
+        inspectionpath=self.project_data['InspectionPath']
+        inspectionside=self.project_data['InspectionSide']
+        mode=self.project_data['CADMode']
+        posxp, posyp=self.pos
+        widthp, heightp = self.size
+        xmin, xmax, ymin, ymax=inspection.get_pp_tool_area(self.project_data)
+        width=xmax-xmin
+        height=ymax-ymin
+
+        if width==0 or height==0:
+            return
+
+        # calculate click position
+        posxp=posxp+widthp*0.01
+        posyp=posyp+heightp*0.01
+
+        # scaling
+        widthp*=0.98
+        heightp*=0.98
+        scale=min(widthp / width, heightp / height)
+
+        touchxp, touchyp=touch.pos
+
+        if inspectionside=="Top":
+            touchxp=touchxp-posxp
+            touchyp=touchyp-posyp
+        else:
+            touchxp=widthp-(touchxp-posxp)
+            touchyp=touchyp-posyp
+
+        xs, ys = inspection.get_pp_position(data, touchxp,touchyp,width*scale,height*scale)
+        # out of image
+        print("pos:", touch.pos, self.pos, self.size, posxp, posyp, "xs ", xs, "ys ", ys, xmin, xmax, ymin, ymax)
+
+        if xs < xmin or xs > xmax or ys < ymin or ys > ymax:
+            return
+        # perform action on mode
+        if mode=="Select":
+            #excellon.select_by_position(soldertoolpath, xnc, ync, selectedsolderingprofile)
+            return
+        elif mode=="Deselect":
+            #excellon.deselect_by_position(soldertoolpath, xnc, ync)
+            return
+        elif mode=="Ref1":
+            #excellon.set_reference_1(soldertoolpath, xnc, ync)
+            return
+        elif mode=="Ref2":
+            #excellon.set_reference_2(soldertoolpath, xnc, ync)
+            return
+        self.redraw_cad_view()
         return
